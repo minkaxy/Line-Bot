fc7bc5a0d2abb811f49bd7d8535a5712f168d1df
minkaxy <pakapon.w@gmail.com>
2017-04-10 00:08:13 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -10,7 +10,7 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'ลุงเก้ง' && $event['ลุงเก้ง']['type'] == 'text') {
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['message']['text'];
 			// Get replyToken
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => "ครวยผิดจังหวะ"
+				'text' => "ทำไงหนิ"
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
